058b66247f9209c6278aa9016eda6ec7eba50882
RAKSHIT1998 <84240184+RAKSHIT1998@users.noreply.github.com>
2026-03-02 12:51:41 +0530

    Adds Swift bridging header to project targets
    Enables interoperability between Swift and Objective-C by specifying a bridging header for relevant targets. Facilitates integration of legacy Objective-C code with newer Swift code.

---
+++ GoFit.Ai - live Healthy/GoFit.Ai - live Healthy-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  GoFit.Ai - live Healthy-Bridging-Header.h
+//  GoFit.Ai - live Healthy
+//
+
+#ifndef GoFit_Ai_live_Healthy_Bridging_Header_h
+#define GoFit_Ai_live_Healthy_Bridging_Header_h
+
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+#endif /* GoFit_Ai_live_Healthy_Bridging_Header_h */
